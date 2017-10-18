a2b912be9c2f94c56b879aea9477862e32f712f5
David Basil <info@basildavid.com>
2017-10-18 07:26:04 +0400

    add homepage rendering

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,11 +1,48 @@
-// home page
-module.exports.locationsList = function(req, res){
-	res.render('index', { title: 'Home' })
+var request = require('request')
+var apiOptions = {
+	server: "http://localhost:3000"
+}
+
+// home page rendering
+var renderHomepage = function(req, res, responseBody){
+	var message
+	if (!(responseBody instanceof Array)){
+		message = "API lookup error"
+		responseBody = []
+	} else {
+		if (!responseBody.length){
+			message = "No places found nearby"
+		}
+	}
+	res.render('locations-list', {
+		title: 'Cafespot - review your favorite cafe.',
+		pageHeader: {
+			title: 'Cafespot',
+			strapline: 'Review your favorite cafe in your town'
+		},
+		locations: responseBody,
+		message: message
+	})
+}
+
+// home page controller
+module.exports.homelist = function(req, res){
+	var requestOptions, path
+	path = '/api/locations'
+	requestOptions = {
+		url: apiOptions.server + path,
+		method: 'GET',
+		json: {},
+		qs: {}
+	}
+	request(requestOptions, function(err, response, body){
+		renderHomepage(req, res, body)
+	})
 }
 
 // single location
-module.exports.location = function(req, res){
-	res.render('location', { title: 'Starcups' })
+module.exports.locationInfo = function(req, res){
+	res.render('location-info', { title: 'Starcups' })
 }
 
 // add review page
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -3,8 +3,8 @@ var router = express.Router()
 var ctrlLocations = require('../controllers/locations')
 
 // locations routes
-router.get('/', ctrlLocations.locationsList)
-router.get('/location', ctrlLocations.location)
+router.get('/', ctrlLocations.homelist)
+router.get('/location/:locationid', ctrlLocations.locationInfo)
 router.get('/location/review/new', ctrlLocations.addReview)
 
 module.exports = router
