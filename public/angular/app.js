a30137cc517547c2d5c33e78aaf445592f0416ce
David Basil <info@basildavid.com>
2017-10-20 12:19:51 +0400

    add $http requests for location list

--- public/angular/app.js
+++ public/angular/app.js
@@ -1,4 +1,15 @@
+var ratingStars = function(){
+	return {
+		scope: {
+			thisRating: '=rating'
+		},
+		templateUrl: '/angular/rating-stars.html'
+	}
+}
+
 angular
 	.module('cafespot', [])
 	.controller('locationListCtrl', locationListCtrl)
+	.directive('ratingStars', ratingStars)
+	.service('cafespotData', cafespotData)
 
--- public/angular/locationListCtrl.js
+++ public/angular/locationListCtrl.js
@@ -1,17 +1,14 @@
-var locationListCtrl = function($scope){
-	$scope.data = {
-		locations: [{
-			title: 'Burger Queen',
-			address: '125 High Street, Reading, WS',
-			rating: 3,
-			facilities: ['Hot drinks', 'Food', 'Premium wifi'],
-			_id: '59e6bea3fd9061171e885cb4'
-		}, {
-			title: 'Gcusine',
-			address: '34 Piedmont Ave, Atlanta, GA',
-			rating: 5,
-			facilities: ['Drinks', 'Wi-fi', 'Music'],
-			_id: '5370a35f2536f6785f8dfb6a'
-		}]
-	}
+var cafespotData = function($http){
+	return $http.get('/api/locations')
 }
+// review $http calls
+var locationListCtrl = function($scope, cafespotData){
+	$scope.message = "Checking your location"
+	cafespotData.then(function(response){
+		$scope.data = { locations: response.data }
+		$scope.message = "Found " + response.data.length + " locations."
+	}, function(e){
+		$scope.message = 'Error!'
+	})
+}
+
