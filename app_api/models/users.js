28d1755283b905d18327295af5e6d9bdbcc61a21
David Basil <info@basildavid.com>
2017-10-24 14:30:07 +0400

    modify .gitignore

--- app_api/models/users.js
+++ app_api/models/users.js
@@ -26,3 +26,15 @@ userSchema.methods.validPassword = function(password){
 	var hash = crypto.pbkdf2sync(password, this.salt, 1000, 64).toString('hex')
 	return this.hash === hash
 }
+
+// JWT
+userSchema.methods.generateJwt = function(){
+	var expiry = new Date()
+	expiry.setDate(expiry.getDate() + 7)
+	return jwt.sign({
+		_id: this._id,
+		email: this.email,
+		name: this.name,
+		exp: pareseInt(expiry.getTime() / 1000),
+	}, 'thisIsSecret')
+}
